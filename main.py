663bea00d03a96d6b0c3b8109709f5d4ee226722
madyaknitto <achmadya.ilyawan.knitto@gmail.com>
2026-02-06 16:30:24 +0700

    add: current work

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
     while 1:
         log_state()
         for event in pygame.event.get():
@@ -18,6 +20,8 @@ def main():
                 return
             screen.fill("black")
             pygame.display.flip()
+        dt = clock.tick(60)
+        print(dt)
 
 
 if __name__ == "__main__":
